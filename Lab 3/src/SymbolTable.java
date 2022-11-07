afbd613556c80d367e2edbcb81c093bcd6a81494
AndreiLupsa <72352373+AndreiLupsa@users.noreply.github.com>
2022-11-07 12:13:13 +0200

    1

---
+++ Lab 3/src/SymbolTable.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Objects;
+
+public class SymbolTable {
+
+    String[] symbols;
+    int size = 0;
+
+    SymbolTable(int max_cap){
+        symbols = new String[max_cap];
+    }
+
+    Boolean exists(String name){
+        for (String s : symbols){
+            if (Objects.equals(s, name)){
+                return true;
+            }
+        }
+        return false;
+    }
+    int add(String name){
+        if(!exists(name)){
+            if(size == symbols.length) {
+                symbols = Arrays.copyOf(symbols, symbols.length * 2);
+            }
+            symbols[size++] = name;
+        }
+        return size-1;
+    }
+    
+    int add(int val){
+        return add(Integer.toString(val));
+    }
+
+    String[] getAll(){
+        return symbols;
+    }
+
+    public String toString(){
+        return Arrays.toString(symbols);
+    }
+}
