f045d060347d922f3898721296d41aa987d0f335
AndreiLupsa <72352373+AndreiLupsa@users.noreply.github.com>
2022-12-04 13:10:00 +0200

    1

--- Lab 3/src/Main.java
+++ Lab 3/src/Main.java
@@ -2,30 +2,25 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-//        MyScanner sc = new MyScanner("C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\P2.txt");
-//        System.out.println(sc.scan());
-        System.out.println();
-        System.out.println("P1");
-        MyScanner scannerP1 = new MyScanner("C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\P1.txt",
-                "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\PIF.txt",
-                "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\ST.txt");
+        System.out.println("P1:");
+        MyScanner scannerP1 = new MyScanner("C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\P1.txt",
+                "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\PIF1.txt",
+                "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\ST1.txt");
         scannerP1.scan();
-//        SymTable sti = new SymTable(1);
-//        SymTable stc = new SymTable(1);
-//        int a = sti.add("a");
-//        int b = sti.add("b");
-//        sti.add("c");
-//        sti.add("c");
-//        int c = sti.add("c");
-//        sti.add("c");
-//        sti.add("c");
-//        int u = stc.add(1);
-//        stc.add(2);
-//        System.out.println(sti);
-//        System.out.println(stc);
-//        System.out.println(a);
-//        System.out.println(b);
-//        System.out.println(c);
-//        System.out.println(u);
+        System.out.println("P1err:");
+        MyScanner scannerP1err = new MyScanner("C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\P1err.txt",
+                "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\PIF1err.txt",
+                "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\ST1err.txt");
+        scannerP1err.scan();
+        System.out.println("P2:");
+        MyScanner scannerP2 = new MyScanner("C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\P2.txt",
+                "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\PIF2.txt",
+                "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\ST2.txt");
+        scannerP2.scan();
+        System.out.println("P3:");
+        MyScanner scannerP3 = new MyScanner("C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\P3.txt",
+                "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\PIF3.txt",
+                "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\ST3.txt");
+        scannerP3.scan();
     }
 }
