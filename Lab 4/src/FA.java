55f987099c976fb8c85cc85849a3663c77d3dcb9
AndreiLupsa <72352373+AndreiLupsa@users.noreply.github.com>
2022-12-06 19:05:13 +0200

    1

---
+++ Lab 4/src/FA.java
@@ -0,0 +1,134 @@
+import java.io.File;
+import java.util.*;
+
+public class FA {
+    public Set<String> alphabet, states, finalStates;
+    public String initialState;
+    public Map< Pair<String, String>, Set<String> > transitions;
+
+    public FA(String filename) {
+        this.states = new HashSet<>();
+        this.alphabet = new HashSet<>();
+        this.finalStates = new HashSet<>();
+        this.transitions = new HashMap<>();
+
+        readFA(filename);
+    }
+
+    private void readFA(String filename){
+        try {
+            File file = new File(filename);
+            Scanner reader = new Scanner(file);
+
+            String statesLine = reader.nextLine();
+            states = new HashSet<>(Arrays.asList(statesLine.split(" ")));
+
+            String alphabetLine = reader.nextLine();
+            alphabet = new HashSet<>(Arrays.asList(alphabetLine.split(" ")));
+
+            initialState = reader.nextLine();
+
+            String finalStatesLine = reader.nextLine();
+            finalStates = new HashSet<>(Arrays.asList(finalStatesLine.split(" ")));
+
+            while(reader.hasNextLine()){
+                String transitionLine = reader.nextLine();
+                String[] transElems = transitionLine.split(" ");
+
+                if(states.contains(transElems[0]) && alphabet.contains(transElems[1]) && states.contains(transElems[2])) {
+
+                    Pair<String, String> trans = new Pair<>(transElems[0], transElems[1]);
+                    System.out.println(trans);
+
+                    if (!(transitions.containsKey(trans))) {
+                        Set<String> transitionStatesSet = new HashSet<>();
+                        transitionStatesSet.add(transElems[2]);
+                        transitions.put(trans, transitionStatesSet);
+                        System.out.println(transitions);
+                    } else {
+                        System.out.println("aaa");
+                        transitions.get(trans).add(transElems[2]);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public boolean checkIfDFA(){
+        return this.transitions.values().stream().noneMatch(list -> list.size() > 1);
+    }
+
+    public boolean checkSequence(String sequence){
+        if(sequence.length() == 0)
+            return finalStates.contains(initialState);
+
+        String state = initialState;
+        for(int i=0;i<sequence.length();++i){
+            Pair<String, String> key = new Pair<>(state, String.valueOf(sequence.charAt(i)));
+            if(transitions.containsKey(key))
+                state = transitions.get(key).iterator().next();
+            else
+                return false;
+        }
+
+        return finalStates.contains(state);
+    }
+
+    public String getAllStates(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("States: ");
+        for (String s : states){
+            builder.append(s).append(" ");
+        }
+
+        return builder.toString();
+    }
+
+    public String getInitialState(){
+        return "Initial state: " +
+                initialState + " ";
+    }
+
+    public String getFinalStates(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Final states: ");
+        for (String fs : finalStates){
+            builder.append(fs).append(" ");
+        }
+
+        return builder.toString();
+    }
+
+    public String getAlphabet(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Alphabet: ");
+        for (String a : alphabet){
+            builder.append(a).append(" ");
+        }
+
+        return builder.toString();
+    }
+
+    public String getTransitions(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Transitions: \n");
+        transitions.forEach((K, V) -> {
+            builder.append("(").append(K.key).append(",").append(K.value).append(") = ").append(V).append("\n");
+        });
+
+        return builder.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "FiniteAutomaton{" +
+                ", states=" + states +
+                ", alphabet=" + alphabet +
+                ", initialState='" + initialState + '\'' +
+                ", finalStates=" + finalStates +
+                ", transitions=" + transitions +
+                '}';
+    }
+}
--- Lab 4/src/Main.java
+++ Lab 4/src/Main.java
@@ -1,7 +1,78 @@
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
+//        p 0 p
+//        p 0 q
+//        p 1 q
+//        q 0 p
+//        q 1 r
+//        r 0 p
+//        r 1 r
+        System.out.println("1. FA");
+        System.out.println("2. Scanner");
+        System.out.println("Command: ");
+
+        Scanner reader = new Scanner(System.in);
+        int option = reader.nextInt();
+
+        switch (option) {
+            case 1 -> runDFA();
+            case 2 -> runScanner();
+        }
+    }
+
+    private static void printDFAMenu(){
+        System.out.println("Menu");
+        System.out.println("1. Display all states.");
+        System.out.println("2. Display initial state.");
+        System.out.println("3. Display final states.");
+        System.out.println("4. Display alphabet.");
+        System.out.println("5. Display transitions.");
+        System.out.println("6. Check sequence.");
+        System.out.println("0. Exit");
+    }
+
+    private static void runDFA(){
+        FA fa = new FA("src/Files/FA.in");
+        printDFAMenu();
+        System.out.println("Command: ");
+
+        Scanner reader = new Scanner(System.in);
+        int option = reader.nextInt();
+
+        while(option != 0) {
+            switch (option) {
+                case 1 -> System.out.println(fa.getAllStates());
+                case 2 -> System.out.println(fa.getInitialState());
+                case 3 -> System.out.println(fa.getFinalStates());
+                case 4 -> System.out.println(fa.getAlphabet());
+                case 5 -> System.out.println(fa.getTransitions());
+                case 6 -> {
+                    if(fa.checkIfDFA()) {
+                        System.out.println("Your sequence: ");
+                        Scanner reader2 = new Scanner(System.in);
+                        String sequence = reader2.next();
+
+                        if (fa.checkSequence(sequence))
+                            System.out.println("Sequence is valid");
+                        else
+                            System.out.println("Invalid sequence");
+                    }
+                    else {
+                        System.out.println("FA is not deterministic.");
+                    }
+                }
+            }
+            System.out.println();
+            printDFAMenu();
+            System.out.println("Command: ");
+            option = reader.nextInt();
+        }
+    }
+
+    private static void runScanner(){
         System.out.println("P1:");
         MyScanner scannerP1 = new MyScanner("C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\P1.txt",
                 "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFTC\\Lab 3\\src\\Files\\PIF1.txt",
